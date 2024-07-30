d7bfd95cda577108d661aff9e8318d8116095df5
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-07-30 08:10:50 -0700

    dashboard controller updates

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -14,18 +14,23 @@ dashboardController.getPokemon = async (req, res, next) => {
     }
 
     try {
-        let fetchedPokemon;
+        let fetchedUrls;;
         // if type, generation, game all selected as 'All', return all pokemon
         if (type === 'All' && generation === 'All' && game === 'All') {
             const allPokemonResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1500'); // 1302 pokemon total, limit 1500 will get all in the fetch
             const allPokemonData = await allPokemonResponse.json();
-            fetchedPokemon = allPokemonData.results.map(pokemon => pokemon.url);
+            fetchedUrls = allPokemonData.results.map(pokemon => pokemon.url);
         } else {
             // if filters pokemon to fetch based on query parameters
-
         }
-
-
+        //after filter, get pokemon data from url
+        //iterate over fetchedUrls and fetch each pokemon's data
+        const fetchedPokemon = {};
+        fetchedUrls.forEach(async (url) => {
+            const pokemonData = {};
+            const response = await fetch(url);
+            const data = await response.json();
+        })
 
     } catch (err) {
         return next({
