f7b280063fa70231009c22fbb1419fc6e4b16ef4
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-07-30 13:59:39 -0700

    starting battle

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -4,7 +4,7 @@ const dashboardController = require('../controllers/dashboardController');
 const authMiddleware = require('../controllers/authMiddleware'); // Import the auth middleware
 
 // Protect the /dashboard route with authMiddleware
-router.get('/', authMiddleware, dashboardController.getPokemon, (req, res) => {
+router.get('/', dashboardController.getPokemon, (req, res) => {
     return res.status(200).send(res.locals.pokemon);
 });
 
