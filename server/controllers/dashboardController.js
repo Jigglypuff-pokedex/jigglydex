cc7008b6a6342841d2eecf02e910176223573a3e
nellysegi <113136845+nellysegi@users.noreply.github.com>
2024-07-30 10:07:11 -0700

    dashboard controller with original properties works

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -4,8 +4,8 @@ const dashboardController = {};
 
 dashboardController.getPokemon = async (req, res, next) => {
     console.log('getPokemon middleware');
-    const { type, generation, game } = req.query;
-    if (!type || !generation || !game) {
+    const { type, generation } = req.query;
+    if (!type || !generation) {
         return next({
             log: 'dashboardController.getPokemon: ERROR: Missing query parameters',
             status: 400,
@@ -13,49 +13,65 @@ dashboardController.getPokemon = async (req, res, next) => {
         });
     }
     try {
-        let fetchedUrls = new Set();
+        let fetchedUrls = [];
         // if type, generation, game all selected as 'All', return all pokemon
-        if (type === 'All' && generation === 'All' && game === 'All') {
+        if (type === 'All' && generation === 'All') {
             //fetching just 100 so run time isn't crazy for demo
-            const allPokemonResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit='); // 1302 pokemon total, limit 1500 will get all in the fetch
+            const allPokemonResponse = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100');
             const allPokemonData = await allPokemonResponse.json();
-            fetchedUrls = new Set(allPokemonData.results.map(pokemon => pokemon.url));
+            fetchedUrls = allPokemonData.results.map(pokemon => pokemon.url);
         } else {
             // if filters pokemon to fetch based on query parameters
-            let typeUrls, generationUrls, gameUrls;
+            let typeUrls = []
+            let generationUrls = [];
             if (type !== 'All') {
                 const typeResponse = await fetch(`https://pokeapi.co/api/v2/type/${type}`);
                 const typeData = await typeResponse.json();
-                typeUrls = new Set(typeData.pokemon.map(pokemon => pokemon.pokemon.url));
+                typeUrls = typeData.pokemon.map(pokemon => pokemon.pokemon.url);
             }
             if (generation !== 'All') {
                 const generationResponse = await fetch(`https://pokeapi.co/api/v2/generation/${generation}`);
                 const generationData = await generationResponse.json();
-                generationUrls = new Set(generationData.pokemon_species.map(pokemon => pokemon.url));
+                const speciesUrls = generationData.pokemon_species.map(pokemon => pokemon.url);
+                
+                const generationPromises = speciesUrls.map(async (url) => {
+                    const response = await fetch(url);
+                    const data = await response.json();
+                    // iterate over each variety and get the pokemon url
+                    data.varieties.forEach(variety => {
+                        generationUrls.push(variety.pokemon.url);
+                    });
+                });
+            
+                await Promise.all(generationPromises);
+                
+                // fetch all pokemon in species, has multiple varieties
+                // speciesUrls.forEach(async (url) => {
+                //     const response = await fetch(url);
+                //     const data = await response.json();
+                //     // iterate over each variety and get the pokemon url
+                //     for (let i = 0; i < data.varieties.length; i++) {
+                //         generationUrls.push(data.varieties[i].pokemon.url);
+                //     }
+                // });
             }
-            if (game !== 'All') {
-                const gameResponse = await fetch(`https://pokeapi.co/api/v2/version/${game}`);
-                const gameData = await gameResponse.json();
-                gameUrls = new Set(gameData.pokemon_species.map(pokemon => pokemon.url));
-            }
+
             //get intersection of fetchedUrls
             if(typeUrls) {
                 fetchedUrls = typeUrls;
             }
             //if fetchedUrls is empty, no need to filter
             if(generationUrls) {
-                fetchedUrls = fetchedUrls.size ? new Set([...fetchedUrls].filter(url => generationUrls.has(url))) : generationUrls;
-            }
-            if(gameUrls) {
-                fetchedUrls = fetchedUrls.size ? new Set([...fetchedUrls].filter(url => gameUrls.has(url))) : gameUrls;
+                fetchedUrls = fetchedUrls.size ? [...fetchedUrls].filter(url => generationUrls.has(url)) : generationUrls;
             }
         }
+        res.locals.pokemon = fetchedUrls;
         //fetchedUrls has all pokemon urls
         //iterate over fetchedUrls and fetch each pokemon's data
         const fetchedPokemon = [];
 
         const fetchAllPokemon = async () => {
-            const fetchPromises = Array.from(fetchedUrls).map(async (url) => {
+            const fetchPromises = fetchedUrls.map(async (url) => {
                 const response = await fetch(url);
                 const data = await response.json();
                 // pokemon name, id, types, image
@@ -63,17 +79,15 @@ dashboardController.getPokemon = async (req, res, next) => {
                     name: data.name,
                     id: data.id,
                     types: data.types.map(type => type.type.name),
-                    image: data.sprites.front_default
+                    image: data.sprites.front_default,
                 };
             });
-        
-            const results = await Promise.all(fetchPromises);
-            fetchedPokemon.push(...results);
-            console.log('fetchedpokemon', fetchedPokemon);
+            const fetchedPokemon = await Promise.all(fetchPromises);
             res.locals.pokemon = fetchedPokemon;
         };
 
-        fetchAllPokemon().then(() => next());
+        await fetchAllPokemon();
+        return next();
     } catch (err) {
         return next({
             log: `dashboardController.getPokemon: ERROR: ${err}`,
